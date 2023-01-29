53537c105ce64fd8f986d0e53caa06cf2453147a
aydar <aydar.baysuvakov@gmail.com>
2023-01-29 22:32:59 +0500

    Объединение функций

--- GameComponents/Character.py
+++ GameComponents/Character.py
@@ -13,170 +13,88 @@ class Character(Object):
     Vx, Vy = 4, 0
     event = None
 
-    def __init__(self, group, pos, image_name=None, form='rect'):
+    def __init__(self, group, pos, window, image_name=None, form='rect'):
         super().__init__(group, image_name, size=[50, 50], take_size=True, colorkey=-1, form=form)
         self.rect.left, self.rect.top = pos
+        self.window = window
 
-class Cube(Character):
-    cube_image = 'cube.png'
-
-    def __init__(self, group, pos):
-        super().__init__(group, pos, ('Color', 'yellow'))
-
-    def get_event(self, events, window):
+    def get_event(self, events):
         for key, value in events.items():
             if key in (pygame.K_UP, pygame.K_SPACE, 1)  and value:
-                if pygame.sprite.spritecollideany(self, window.platforms):
-                    self.jump()
-        self.move(window)
+                self.jump()
+        self.move()
 
-    def jump(self):
-        self.Vy -= 7
+    def move(self):
+        self.rect = self.rect.move(0, sign(self.G))
+        self.falling()
+        self.running()
+        self.rect = self.rect.move(0, -sign(self.G))
 
-    def move(self, window):
-        self.rect = self.rect.move(0, -1)
-        self.falling(window)
-        self.running(window)
-        self.rect = self.rect.move(0, 1)
-
-    def falling(self, window):
+    def falling(self):
         dir, mod = sign(self.Vy), int(abs(self.Vy))
         for i in range(mod):
             self.rect = self.rect.move(0, dir)
-            if pygame.sprite.spritecollideany(self, window.platforms):
+            if pygame.sprite.spritecollideany(self, self.window.platforms):
                 self.rect = self.rect.move(0, -dir)
                 self.Vy = 0
                 return 0
         self.Vy -= self.G
 
-    def running(self, window):
+    def running(self):
         dir, mod = sign(self.Vx), int(abs(self.Vx))
         for i in range(mod):
             self.rect = self.rect.move(dir, 0)
-            if pygame.sprite.spritecollideany(self, window.platforms):
+            if pygame.sprite.spritecollideany(self, self.window.platforms):
                 self.rect = self.rect.move(-dir, 0)
                 self.event = 'restart'
+
+class Cube(Character):
+    cube_image = 'cube.png'
+
+    def __init__(self, group, pos, window):
+        super().__init__(group, pos, window,('Color', 'yellow'))
+
+    def jump(self):
+        if pygame.sprite.spritecollideany(self, self.window.platforms):
+            self.Vy += 7 * sign(self.G)
 
 class Ufo(Character):
     ufo_image = 'ufo.png'
     hold = False
 
-    def __init__(self, group, pos):
-        super().__init__(group, pos, ('Color', 'black'))
+    def __init__(self, group, pos, window):
+        super().__init__(group, pos, window, ('Color', 'black'))
 
-    def get_event(self, events, window):
+    def get_event(self, events):
         for key, value in events.items():
-            if key in (pygame.K_UP, pygame.K_SPACE, 1) and value and not self.hold:
+            if key in (pygame.K_UP, pygame.K_SPACE, 1) and value:
                 self.jump()
-                self.hold = True
             if key in (pygame.K_UP, pygame.K_SPACE, 1) and not value:
                 self.hold = False
-        self.move(window)
+        self.move()
 
     def jump(self):
-        self.Vy = -5
-
-    def move(self, window):
-        self.rect = self.rect.move(0, -1)
-        self.falling(window)
-        self.running(window)
-        self.rect = self.rect.move(0, 1)
-
-    def falling(self, window):
-        dir, mod = sign(self.Vy), int(abs(self.Vy))
-        for i in range(mod):
-            self.rect = self.rect.move(0, dir)
-            if pygame.sprite.spritecollideany(self, window.platforms):
-                self.rect = self.rect.move(0, -dir)
-                self.Vy = 0
-                return 0
-        self.Vy -= self.G
-
-    def running(self, window):
-        dir, mod = sign(self.Vx), int(abs(self.Vx))
-        for i in range(mod):
-            self.rect = self.rect.move(dir, 0)
-            if pygame.sprite.spritecollideany(self, window.platforms):
-                self.rect = self.rect.move(-dir, 0)
-                self.event = 'restart'
+        if not self.hold:
+            self.hold = True
+            self.Vy = 7 * sign(self.G)
 
 class Ball(Character):
     ball_image = 'ball.png'
     G = -0.4
 
-    def __init__(self, group, pos):
-        super().__init__(group, pos, ('Color', 'red'), form='circle')
-
-    def get_event(self, events, window):
-        for key, value in events.items():
-            if key in (pygame.K_UP, pygame.K_SPACE, 1) and value:
-                if pygame.sprite.spritecollideany(self, window.platforms):
-                    self.jump()
-        self.move(window)
+    def __init__(self, group, pos, window):
+        super().__init__(group, pos, window, ('Color', 'red'), form='circle')
 
     def jump(self):
-        self.G *= -1
-        self.rect = self.rect.move(0, -sign(self.G) * 2)
-
-    def move(self, window):
-        self.rect = self.rect.move(0, sign(self.G))
-        self.falling(window)
-        self.running(window)
-        self.rect = self.rect.move(0, -sign(self.G))
-
-    def falling(self, window):
-        dir, mod = sign(self.Vy), int(abs(self.Vy))
-        for i in range(mod):
-            self.rect = self.rect.move(0, dir)
-            if pygame.sprite.spritecollideany(self, window.platforms):
-                self.rect = self.rect.move(0, -dir)
-                self.Vy = 0
-                return 0
-        self.Vy -= self.G
-
-    def running(self, window):
-        dir, mod = sign(self.Vx), int(abs(self.Vx))
-        for i in range(mod):
-            self.rect = self.rect.move(dir, 0)
-            if pygame.sprite.spritecollideany(self, window.platforms):
-                self.rect = self.rect.move(-dir, 0)
-                self.event = 'restart'
+        if pygame.sprite.spritecollideany(self, self.window.platforms):
+            self.G *= -1
+            self.rect = self.rect.move(0, -sign(self.G) * 2)
 
 class Ship(Character):
     ship_image = 'ship.png'
 
-    def __init__(self, group, pos):
-        super().__init__(group, pos, ('Color', 'black'))
-
-    def get_event(self, events, window):
-        for key, value in events.items():
-            if key in (pygame.K_UP, pygame.K_SPACE, 1)  and value:
-                self.jump()
-        self.move(window)
+    def __init__(self, group, pos, window):
+        super().__init__(group, pos, window, ('Color', 'black'))
 
     def jump(self):
-        self.Vy -= 0.6
-
-    def move(self, window):
-        self.rect = self.rect.move(0, -1)
-        self.falling(window)
-        self.running(window)
-        self.rect = self.rect.move(0, 1)
-
-    def falling(self, window):
-        dir, mod = sign(self.Vy), int(abs(self.Vy))
-        for i in range(mod):
-            self.rect = self.rect.move(0, dir)
-            if pygame.sprite.spritecollideany(self, window.platforms):
-                self.rect = self.rect.move(0, -dir)
-                self.Vy = 0
-                return 0
-        self.Vy -= self.G
-
-    def running(self, window):
-        dir, mod = sign(self.Vx), int(abs(self.Vx))
-        for i in range(mod):
-            self.rect = self.rect.move(dir, 0)
-            if pygame.sprite.spritecollideany(self, window.platforms):
-                self.rect = self.rect.move(-dir, 0)
-                self.event = 'restart'
+        self.Vy -= 0.5
